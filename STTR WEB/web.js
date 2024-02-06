1f7ca09cca349c949909ad2567b03a0fa7755ce6
clbsangtaotreLTVq1 <159134262+clbsangtaotreLTVq1@users.noreply.github.com>
2024-02-06 17:53:45 +0700

    Add files via upload

---
+++ STTR WEB/web.js
@@ -0,0 +1,9 @@
+let nhap;
+let du_dieu;
+
+document.getElementById("btn").onclick = function(){
+    nhap = document.getElementById("search").value;
+    console.log(nhap);
+    du_lieu = document.getElementsByClassName("container").value;
+    console.log(du_lieu)
+}
